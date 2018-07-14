60ca4034a8763865866bf5c6326648fe66df5288
Drew Miller <poppiestar@users.noreply.github.com>
2018-07-14 10:43:22 +0100

    Install and configure redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React, { SFC } from "react";
+import { Provider } from "react-redux";
 import { HomeScreen } from "./components/screens";
+import store from "./redux/store";
 
 const App: SFC<{}> = () => (
-    <HomeScreen />
+    <Provider store={store}>
+        <HomeScreen />
+    </Provider>
 );
 
 export default App;
---
+++ src/redux/game/game.reducer.ts
@@ -0,0 +1,31 @@
+import { SET_VISIBILITY_FILTER } from "./game.actions";
+
+interface IGameStore {
+    visibilityFilter: string;
+}
+
+interface IGameAction {
+    type: string;
+    payload: any;
+}
+
+export const initialState: IGameStore = {
+    visibilityFilter: "off",
+};
+
+const gameReducer = (state: IGameStore = initialState, action: IGameAction): IGameStore => {
+    switch (action.type) {
+        case SET_VISIBILITY_FILTER:
+            return setVisibilityFilter(state, action);
+
+        default:
+            return state;
+    }
+};
+
+export default gameReducer;
+
+const setVisibilityFilter = (state: IGameStore, { payload }: IGameAction): IGameStore => ({
+    ...state,
+    visibilityFilter: payload
+});
