48191ff35837edf217dc68d00d4640e5f9110e3a
Drew Miller <poppiestar@users.noreply.github.com>
2018-07-16 14:05:00 +0100

    Testing is almost working

---
+++ src/components/screens/home/home.screen.spec.tsx
@@ -0,0 +1,18 @@
+import { shallow } from "enzyme";
+import React from "react";
+import "react-native";
+import HomeScreen from "./home.screen";
+
+describe("HomeScreen", () => {
+
+    it("should render", () => {
+
+        const actual = shallow(
+            <HomeScreen
+                setVisibilityFilter={jest.fn()}
+                visibilityFilter="maybe"
+            />
+        );
+        expect(actual).toMatchSnapshot();
+    });
+});
