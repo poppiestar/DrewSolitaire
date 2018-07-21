bbb0017e0771fe1464e1994b43e2dd7b4527eae5
Drew Miller <poppiestar@users.noreply.github.com>
2018-07-21 23:56:03 +0100

    Add types for card-deck

---
+++ typings/declarations.d.ts
@@ -0,0 +1,22 @@
+
+declare module "card-deck" {
+    class Deck {
+        addRandom: (cards: any[]) => Deck;
+        addToBottom: (cards: any[]) => Deck;
+        addToTop: (cards: any[]) => Deck;
+        bottom: (count: number) => any[];
+        cards: (cards: any[]) => Deck;
+        draw: (count: number) => any[];
+        drawFromBottom: (count: number) => any[];
+        drawRandom: (count: number) => any[];
+        drawWhere: (count: number) => any[];
+        random: (count: number) => any[];
+        remaining: () => number;
+        shuffle: () => Deck;
+        shuffleToBottom: (cards: any[]) => Deck;
+        shuffleToTop: (cards: any[]) => Deck;
+        top: (count: number) => any[];
+    }
+
+    export default Deck;
+}
